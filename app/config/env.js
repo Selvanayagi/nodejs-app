811e8db007a7faef3acc4c6e6eb36db0af1d59df
yogesh056 <yogeshwaran.ramesh@codingmart.com>
2020-11-24 11:18:38 +0530

    upadte

---
+++ app/config/env.js
@@ -0,0 +1,16 @@
+const env = {
+    database: 'd21nu4n5co4f0j',
+    username: 'qpnbimtbtqhjdq',
+    password: '7c067393fa99edc51aeab9458fb3e9b89e0faf7f0c31b667b59bac8d6820fe4f',
+    host: 'ec2-52-1-95-247.compute-1.amazonaws.com',
+    port: 5432,
+    dialect: 'postgres',
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000
+    }
+  };
+   
+  module.exports = env;
